a6b2231e1d7ad0c174d08671570444691b94b1e0
Siddhartha Purwar <siddpurwar@gmail.com>
2023-12-28 23:05:10 +0530

    Sorting: Bubble Sort

--- Sorting Algorithms/Bubble Sort.py
+++ Sorting Algorithms/Bubble Sort.py
@@ -28,3 +28,14 @@ print(f"Before: {lst}\nAfter: {bubble_sort(lst)}")
 #   Best Case: O(1)
 ##################################################
 
+
+# ##################################################
+# When to use bubble sort?
+    # Small datasets.
+    # When simplicity is more important than efficiency.
+    # When the data is already partially sorted.
+
+# When not to use bubble sort?
+    # Avoid bubble sort for large datasets and performance-critical applications.
+    # In such cases, sorting algorithms like merge sort or quick sort are more suitable.
+##################################################
