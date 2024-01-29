e0c6371acf2747dbd8c7b533767d941cd4bf8d75
Siddhartha Purwar <siddpurwar@gmail.com>
2024-01-29 20:11:41 +0530

    HASHING: Quadratic Probing

---
+++ Hashing/Double Hashing.py
@@ -0,0 +1,36 @@
+class DoubleHashing:
+
+    def __init__(self, size):
+        self.table = [None] * size
+    
+    def H1(self, data):
+        return data % len(self.table)   
+
+    def H2(self, data):
+        return 1 + data % (len(self.table) - 1)
+
+    def H(self, data, i):
+        return self.H1(data) + (i * self.H2(data))
+
+    def insert(self, data):
+        i = 0
+        idx = self.H(data, i)
+
+        while self.table[idx] is not None:
+            i += 1
+            idx = self.H(data, i)
+        
+        self.table[idx] = data
+    
+    def retrieve(self, data):
+        i = 0
+        idx = self.H(data, i)
+
+        while self.table[idx] is not None:
+            if self.table[idx] == data:
+                return data
+            else:
+                i += 1
+                idx = self.H(data, i)
+        
+        return None
---
+++ Hashing/Quadratic Probing.py
@@ -0,0 +1,30 @@
+class QuadraticProbing:
+
+    def __init__(self, size):
+        self.table = [None] * size
+    
+    def H(self, data, i):
+        return (data + (i * i)) % len(self.table)
+
+    def insert(self, data):
+        i = 0
+        idx = self.H(data, i)
+
+        while self.table[idx] is not None:
+            i += 1
+            idx = self.H(data, i)
+        
+        self.table[idx] = data
+    
+    def retrieve(self, data):
+        i = 0
+        idx = self.H(data, i)
+
+        while self.table[idx] is not None:
+            if self.table[idx] == data:
+                return data
+            else:
+                i += 1
+                idx = self.H(data, i)
+        
+        return None
