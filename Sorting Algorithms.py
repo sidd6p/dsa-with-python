b4ca6a697d5b1efa2ef34f682f6c5cf51e8c4bf3
Siddhartha Purwar <siddpurwar@gmail.com>
2025-04-22 20:51:08 +0530

    REFACTOR: sort

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -10,6 +10,7 @@ Sorting Algorithms and Time Complexities
 | Quick Sort     | O(n log n)   | O(n log n)   | O(n^2)      |
 """
 
+
 def bubble_sort(nums):
     n = len(nums)
     for i in range(n):
@@ -17,6 +18,7 @@ def bubble_sort(nums):
             if nums[j] > nums[j + 1]:
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
     return nums
+
 
 def insertion_sort(nums):
     n = len(nums)
@@ -29,6 +31,7 @@ def insertion_sort(nums):
         nums[j + 1] = key
     return nums
 
+
 def selection_sort(nums):
     n = len(nums)
     for i in range(n):
@@ -39,41 +42,44 @@ def selection_sort(nums):
         nums[min_idx], nums[i] = nums[i], nums[min_idx]
     return nums
 
-# Merge Sort
-def merge_sort(arr):
-    """Sorts an array using Merge Sort algorithm."""
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = merge_sort(arr[:mid])
-        right_half = merge_sort(arr[mid:])
-        return merge(left_half, right_half)
-    return arr  # Time Complexity: O(n log n) in all cases
-
 
 def merge(left, right):
-    """Merges two sorted halves."""
-    sorted_arr = []
+    combined = list()
     i = j = 0
-    while i < len(left) and j < len(right):
+    l, r = len(left), len(right)
+
+    while i < l and j < r:
         if left[i] < right[j]:
-            sorted_arr.append(left[i])
+            combined.append(left[i])
             i += 1
         else:
-            sorted_arr.append(right[j])
+            combined.append(right[j])
             j += 1
-    sorted_arr.extend(left[i:])
-    sorted_arr.extend(right[j:])
-    return sorted_arr
+
+    if i < l:
+        combined.extend(left[i:])
+    if j < r:
+        combined.extend(right[j:])
+
+    return combined
 
 
-# Quick Sort
-def quick_sort(arr):
-    """Sorts an array using Quick Sort algorithm."""
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quick_sort(left) + middle + quick_sort(right)
-    # Time Complexity: O(n log n) average, O(n^2) worst (bad pivot selection)
+def merge_sort(nums):
+    n = len(nums)
+    if n > 1:
+        left = merge_sort(nums[: n // 2])
+        right = merge_sort(nums[n // 2 :])
+        return merge(left, right)
+    return nums
+
+
+def quick_sort(nums):
+    n = len(nums)
+    if n > 1:
+        mid = nums[n // 2]
+        left = [num for num in nums if num < mid]
+        right = [num for num in nums if num > mid]
+        mid = [num for num in nums if num == mid]
+        return quick_sort(left) + mid + quick_sort(right)
+    else:
+        return nums
