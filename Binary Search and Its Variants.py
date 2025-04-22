b897719164b60f89d358826396962ef23e37705c
Siddhartha Purwar <siddpurwar@gmail.com>
2025-04-22 21:03:18 +0530

    REFACTOR: binary

--- Binary Search and Its Variants.py
+++ Binary Search and Its Variants.py
@@ -8,89 +8,65 @@ Binary Search and Its Variants (Iterative & Recursive)
 | Last Occurrence (Iterative & Recursive) | O(1)      | O(log n)     | O(log n)   |
 """
 
-# Binary Search (Iterative)
-def binary_search(nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = left + (right - left) // 2
+
+def binary_search_iterative(nums, target):
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] < target:
-            left = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
         else:
-            right = mid - 1
+            l = mid + 1
+
     return -1
 
 
-# Binary Search (Recursive)
-def binary_recursive(nums, target, left, right):
-    if left > right:
+def binary_search_recursive(nums, target, l, r):
+    if l > r:
         return -1
 
-    mid = left + (right - left) // 2
-    if nums[mid] == target:
+    mid = l + (r - l) // 2
+
+    if target == nums[mid]:
         return mid
-    elif nums[mid] > target:
-        return binary_recursive(nums, target, left, mid - 1)
+    elif target > nums[mid]:
+        return binary_search_recursive(nums, target, mid + 1, r)
     else:
-        return binary_recursive(nums, target, mid + 1, right)
+        return binary_search_recursive(nums, target, l, mid - 1)
 
 
-# First Occurrence (Iterative)
 def first_occurrence(nums, target):
-    left, right = 0, len(nums) - 1
+    l, r = 0, len(nums) - 1
     sol = -1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if nums[mid] == target:
+
+    while l <= r:
+        mid = l + (r - l) // 2
+        if target == nums[mid]:
+            r = mid - 1
             sol = mid
-            right = mid - 1
-        elif nums[mid] < target:
-            left = mid + 1
+        elif target > nums[mid]:
+            l = mid + 1
         else:
-            right = mid - 1
+            r = mid - 1
+
     return sol
 
 
-# First Occurrence (Recursive)
-def first_occurrence_recursive(nums, target, left, right, sol=-1):
-    if left > right:
-        return sol
-
-    mid = left + (right - left) // 2
-    if nums[mid] == target:
-        return first_occurrence_recursive(nums, target, left, mid - 1, mid)
-    elif nums[mid] > target:
-        return first_occurrence_recursive(nums, target, left, mid - 1, sol)
-    else:
-        return first_occurrence_recursive(nums, target, mid + 1, right, sol)
-
-
-# Last Occurrence (Iterative)
 def last_occurrence(nums, target):
-    left, right = 0, len(nums) - 1
+    l, r = 0, len(nums) - 1
     sol = -1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if nums[mid] == target:
+
+    while l <= r:
+        mid = l + (r - l) // 2
+        if target == nums[mid]:
+            l = mid + 1
             sol = mid
-            left = mid + 1
-        elif nums[mid] < target:
-            left = mid + 1
+        elif target > nums[mid]:
+            l = mid + 1
         else:
-            right = mid - 1
+            r = mid - 1
+
     return sol
-
-
-# Last Occurrence (Recursive)
-def last_occurrence_recursive(nums, target, left, right, sol=-1):
-    if left > right:
-        return sol
-
-    mid = left + (right - left) // 2
-    if nums[mid] == target:
-        return last_occurrence_recursive(nums, target, mid + 1, right, mid)
-    elif nums[mid] > target:
-        return last_occurrence_recursive(nums, target, left, mid - 1, sol)
-    else:
-        return last_occurrence_recursive(nums, target, mid + 1, right, sol)
