542d96ff758461725ea735f94427ad1d34f69f7a
Siddhartha Purwar <siddpurwar@gmail.com>
2025-09-23 11:14:00 +0530

    Update Sorting Algorithms.py

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -3,7 +3,7 @@ Sorting Algorithms and Time Complexities
 
 | Algorithm      | Best Case    | Average Case | Worst Case  |
 |----------------|--------------|--------------|-------------|
-| Bubble Sort    | O(n)         | O(n^2)       | O(n^2)      |
+| Bubble Sort    | O(n^2)         | O(n^2)       | O(n^2)      |
 | Insertion Sort | O(n)         | O(n^2)       | O(n^2)      |
 | Selection Sort | O(n^2)       | O(n^2)       | O(n^2)      |
 | Merge Sort     | O(n log n)   | O(n log n)   | O(n log n)  |
