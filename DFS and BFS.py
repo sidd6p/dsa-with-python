407190428a7033c89b072b1399484e09b60af1cc
Siddhartha Purwar <siddpurwar@gmail.com>
2025-09-28 14:30:50 +0530

    Update DFS and BFS.py

--- DFS and BFS.py
+++ DFS and BFS.py
@@ -1,16 +1,15 @@
 """
 DFS and BFS Search (Iterative & Recursive)
 
-| Algorithm                | Best Case | Average Case | Worst Case |
-|--------------------------|-----------|--------------|------------|
-| DFS (Iterative & Recursive) | O(1)      | O(V + E)     | O(V + E)   |
-| BFS (Iterative & Recursive) | O(1)      | O(V + E)     | O(V + E)   |
+| Algorithm | Best Case | Average Case | Worst Case |
+|-----------|-----------|--------------|------------|
+| DFS       | O(1)      | O(V + E)     | O(V + E)   |
+| BFS       | O(1)      | O(V + E)     | O(V + E)   |
 """
 
 from collections import deque
 
-# Depth-First Search (DFS) - Iterative
-def dfs_iterative(graph, start, end):
+def dfs(graph, start, end):
     if start not in graph or end not in graph:
         return False  
 
@@ -26,31 +25,9 @@ def dfs_iterative(graph, start, end):
 
     return False
 
-    # Time Complexity: O(V + E)
 
 
-# Depth-First Search (DFS) - Recursive
-def dfs_recursive(graph, start, target, visited=None):
-    if start not in graph or target not in graph:
-        return False
-
-    if visited is None:
-        visited = set()
-    if start == target:
-        return True
-    if start in visited:
-        return False
-
-    visited.add(start)
-    for neighbor in graph.get(start, []):
-        if dfs_recursive(graph, neighbor, target, visited):
-            return True
-    return False
-    # Time Complexity: O(V + E)
-
-
-# Breadth-First Search (BFS) - Iterative
-def bfs_iterative(graph, start, target):
+def bfs(graph, start, target):
     if start not in graph or end not in graph:
         return False 
 
@@ -66,28 +43,3 @@ def bfs_iterative(graph, start, target):
             queue.extend(graph.get(current, []))
 
     return False
-    # Time Complexity: O(V + E)
-
-
-# Breadth-First Search (BFS) - Recursive
-def bfs_recursive(graph, queue, target, visited=None):
-    if start not in graph or target not in graph:
-        return False
-
-    if visited is None:
-        visited = set()
-    if queue is None:
-        queue = deque([start])
-    if not queue:
-        return False  
-
-    current = queue.popleft()
-    if current == target:
-        return True
-
-    if current not in visited:
-        visited.add(current)
-        queue.extend(graph.get(current, []))
-
-    return bfs_recursive(graph, start, target, queue, visited)
-    # Time Complexity: O(V + E)
