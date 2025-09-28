56d53f6a7530e907c96836fed122bc6539e8e9ce
Siddhartha Purwar <siddpurwar@gmail.com>
2025-09-28 14:23:26 +0530

    Update Binary Search and Its Variants.py

--- Binary Search and Its Variants.py
+++ Binary Search and Its Variants.py
@@ -1,15 +1,15 @@
 """
 Binary Search and Its Variants (Iterative & Recursive)
 
-| Algorithm                          | Best Case | Average Case | Worst Case |
-|------------------------------------|-----------|--------------|------------|
-| Binary Search (Iterative & Recursive) | O(1)      | O(log n)     | O(log n)   |
-| First Occurrence (Iterative & Recursive) | O(1)      | O(log n)     | O(log n)   |
-| Last Occurrence (Iterative & Recursive) | O(1)      | O(log n)     | O(log n)   |
+| Algorithm        | Best Case | Average Case | Worst Case |
+|------------------|-----------|--------------|------------|
+| Binary Search    | O(1)      | O(log n)     | O(log n)   |
+| First Occurrence | O(1)      | O(log n)     | O(log n)   |
+| Last Occurrence  | O(1)      | O(log n)     | O(log n)   |
 """
 
 
-def binary_search_iterative(nums, target):
+def binary_search(nums, target):
     l, r = 0, len(nums) - 1
 
     while l <= r:
@@ -22,20 +22,6 @@ def binary_search_iterative(nums, target):
             l = mid + 1
 
     return -1
-
-
-def binary_search_recursive(nums, target, l, r):
-    if l > r:
-        return -1
-
-    mid = l + (r - l) // 2
-
-    if target == nums[mid]:
-        return mid
-    elif target > nums[mid]:
-        return binary_search_recursive(nums, target, mid + 1, r)
-    else:
-        return binary_search_recursive(nums, target, l, mid - 1)
 
 
 def first_occurrence(nums, target):
